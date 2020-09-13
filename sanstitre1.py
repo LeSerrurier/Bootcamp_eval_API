fc2516d5f1d30096de29bc89f2d05997c044b71a
LeSerrurier <65590507+LeSerrurier@users.noreply.github.com>
2020-09-13 22:11:33 +0200

    Delete sanstitre1.py

--- sanstitre1.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-bdd = sqlite3.connect('BDD/paulEmploi.db')
-
-res = bdd.execute("""SELECT * FROM entreprise as ent, personne as per, embaucher as emb
-                     WHERE ent.id = emb.idEntreprise
-                     AND per.id = emb.idPersonne
-                     AND ent.nom LIKE 'Airbus'""").fetchone()
-print(res[0])
